921b30506d3ddcc9c256318538b6a64d3c18f850
megaputri19 <86546870+megaputri19@users.noreply.github.com>
2023-06-22 21:31:14 +0700

    Delete lib directory

--- lib/routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-final Map<String, WidgetBuilder> routes = {
-  //LoginScreens.routeName: (context) => LoginScreens(),
-  //RegisterScreen.routeName: (context) => RegisterScreen()
-};
